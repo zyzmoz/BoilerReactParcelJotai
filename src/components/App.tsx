21accff29f17e48382d0bc720bcadcaf68f22107
Daniel Cunha <daniel.cunha@kaffa.com.br>
2020-10-09 17:21:52 -0300

    UI, css and build fix

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,21 @@
-import { atom, useAtom } from 'jotai';
+import { useAtom } from 'jotai';
+import { Container, Button } from '@material-ui/core';
 import React from 'react';
 import { countAtom } from '~atoms';
+import Helmet  from 'react-helmet';
 
 export const App: React.FC = () => {
     const [count, setCount] = useAtom(countAtom);
 
     return (
-        <div>
+        <Container
+            style={{height: "100vh", display: "flex", alignItems: "center", justifyContent:"center", flexDirection:"column"}} 
+        >
+            <Helmet title="App View"/>
             <div>
                 <b>{count}</b>
             </div>
-            <button onClick={() => setCount(count + 1)}>Click</button>
-        </div>
+            <Button variant="contained" color="primary" onClick={() => setCount(count + 1)}>Click</Button>
+        </Container>
     )
 }
